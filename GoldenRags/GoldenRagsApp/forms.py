c0a2be1179e7b28c604f1ea9b15cba08d99f44bc
Aljosa <janjic_aljosa@hotmail.com>
2024-04-02 17:36:11 +0200

    Adding Form style and fields

--- GoldenRags/GoldenRagsApp/forms.py
+++ GoldenRags/GoldenRagsApp/forms.py
@@ -4,4 +4,6 @@ class ScrapeForm(forms.Form):
     product_url = forms.URLField(label='Product URL')
     item_color = forms.CharField(label='Item Color')
     item_size = forms.CharField(label='Item Size')
+    receivers_email = forms.CharField(label='Email for notification')
     send_sms = forms.BooleanField(label='Send SMS', required=False)
+    phone_number = forms.CharField(label='Phone for SMS', required=False)

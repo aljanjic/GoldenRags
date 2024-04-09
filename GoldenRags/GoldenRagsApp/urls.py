25585e8b9c1a4873a4615d21449002d437f58f96
Aljosa <janjic_aljosa@hotmail.com>
2024-04-09 21:39:23 +0200

    Setting redirect from /goldenrags

--- GoldenRags/GoldenRagsApp/urls.py
+++ GoldenRags/GoldenRagsApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('goldenrags/', RedirectView.as_view(url='/goldenrags/scrape/'), name='go-to-scrape'),
     path('goldenrags/scrape/', views.scrape_view , name='scrape'),
 ]
